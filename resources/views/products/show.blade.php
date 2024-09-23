9794c0850ee1a74ae0b801a7f4f326f71c2c5126
OUSSAMA <oussamaamalou2003@gmail.com>
2024-09-23 11:11:13 +0100

    change in show

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -5,7 +5,7 @@
         <h1 class="text-3xl font-bold mb-6">{{ $product->name }}</h1>
 
         <div class="bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4">
-            <p class="text-gray-700 text-lg mb-4"><strong>Price:</strong> ${{ $product->price }}</p>
+            <p class="text-gray-700 text-lg mb-4"><strong>Price : </strong> ${{ $product->price }}</p>
             <p class="text-gray-700 text-lg mb-4"><strong>Category:</strong> {{ $product->category->name }}</p>
 
             <a href="{{ route('products.index') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">

a5aff2046d0155a7d6b1262d9f83c8c371c3a485
OUSSAMA <oussamaamalou2003@gmail.com>
2024-09-23 11:06:11 +0100

    new commit

--- resources/views/products/selling.blade.php
+++ resources/views/products/selling.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container mx-auto py-8">
-        <h1 class="text-4xl font-extrabold mb-8 text-gray-800">Selling Products</h1>
+        <h1 class="text-4xl font-extrabold mb-8 text-gray-800">Selling Product</h1>
 
         <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
             <!-- Products Section on the left -->
